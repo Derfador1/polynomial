80d62e893086f6f428a4c268b60eadb3d3666916
Derfador1 <derfador1@gmail.com>
2016-01-21 18:36:06 -0500

    starter code

---
+++ polynomial.c
@@ -0,0 +1,42 @@
+#include "polynomial.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct term *make_term(int coeff, int exp) 
+{
+	struct term *node = malloc(sizeof(*node));
+	if(node) {
+		node->coeff = coeff;
+		node->exp = exp;
+		node->next = NULL;
+	}
+	return node;
+}
+
+void poly_free(polynomial *eqn)
+{
+	while(eqn) {
+		struct term *tmp = eqn->next;
+		free(eqn);
+		eqn = tmp;
+	}
+}
+
+void poly_print(polynomial *eqn)
+{
+	if(!eqn) {
+		return;
+	}
+	if(eqn->coeff) {
+		printf("%c%d", eqn->coeff > 0 ? '+' : '\0', eqn->coeff);
+		if(eqn->exp > 1) {
+			printf("x^%d", eqn->exp);	
+		}
+		else if(eqn->exp == 1) {
+			printf("x");
+		}
+		printf(" ");
+	}
+	poly_print(eqn->next);
+}
