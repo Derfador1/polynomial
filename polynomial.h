844eabf501cad6d1027b84e308f535133ac22c53
Derfador1 <derfador1@gmail.com>
2016-01-22 20:24:50 -0500

    added the eval poly function to my header file

--- polynomial.h
+++ polynomial.h
@@ -19,3 +19,4 @@ polynomial *add_poly(polynomial *a, polynomial *b);
 polynomial *sub_poly(polynomial *a, polynomial *b);
 bool is_equal(polynomial *a, polynomial *b);
 void apply_to_each_term(polynomial *p, void (*transform)(struct term *));
+double eval_poly(polynomial *p, double x);
